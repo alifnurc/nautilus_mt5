814387cd54201bd63bb398988977398ad83c82ab
hitagi <alifnc@proton.me>
2025-11-03 23:18:24 +0700

    feat: Containerization for consistent environment - Add save_path parameter for download function - Use data dir for downloaded dataticks and parquet soon - Add some better documentation

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Quantitative Trading
+
 This repository contains code and resources related to quantitative trading strategies, data analysis, and algorithmic trading. It includes implementations of various trading algorithms, backtesting frameworks, and tools for data visualization and analysis.
+
+## Learning Milestones
+
+- [x] Simple backtest strategy from builtin example with my own datatick from broker
+- [ ] Writing an adapter to start paper trading
+- [ ] Write my own ICT model strategy
+- [ ] Backtest a year of data
+- [ ] Start live trading with small cent account
+
+## Project structure
+
+```
+QuantNautilus
+├── CHANGELOG.md
+├── data # Data directory containing datatick and parquet
+│   └── dataticks
+│       ├── Exness_EURUSDc_2025_09.csv
+│       └── Exness_EURUSDc_2025_09.zip
+├── docker-compose.yml
+├── Dockerfile
+├── README.md
+├── requirements.txt
+└── src
+    ├── main.py # Backtest script
+    └── strategies # Strategies that use on backtest and live trading
+```
+
+## Prerequisites
+
+- Docker or Podman
+
+## Running with Docker or Podman
+
+```bash
+docker-compose up --build
+
+# or
+
+podman-compose up --build
+```
+
+## Running without Docker and Podman
+
+```bash
+git clone <this repo>
+cd <this repo>
+pip install -r requirements.txt
+python3 src/main.py
+```
--- src/main.py
+++ src/main.py
@@ -16,23 +16,48 @@ from nautilus_trader.model.currencies import USD
 from nautilus_trader.persistence.wranglers import BarDataWrangler
 from nautilus_trader.test_kit.providers import TestInstrumentProvider
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
 
-def download(url: str) -> None:
-    filename = url.rsplit("/", maxsplit=1)[1]
 
-    with open(filename, "wb") as f:
+def download(url: str, save_dir: Path | str, filename: str | None = None) -> Path:
+    save_dir = Path(save_dir)
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    if filename is None:
+        filename = url.rsplit("/", maxsplit=1)[1]
+
+    save_path = save_dir / filename
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(save_path, "wb") as f:
         f.write(requests.get(url).content)
+
+    return save_path
 
 
 if __name__ == "__main__":
-    # Step 1: Configure and create backtest engine
+    # Step 1: Download the datatick
+    url = (
+        "https://ticks.ex2archive.com/ticks/EURUSDc/2025/09/Exness_EURUSDc_2025_09.zip"
+    )
+    save_path = DATA_DIR / "dataticks"
+    download(url, save_path)
+
+    # Step 2: Extract downloaded datatick
+    with zipfile.ZipFile(save_path / "Exness_EURUSDc_2025_09.zip", "r") as zip_ref:
+        zip_ref.extractall(save_path)
+
+    # Step 3: Configure and create backtest engine
     engine_config = BacktestEngineConfig(
         trader_id=TraderId("BACKTEST_TRADER-001"),
         logging=LoggingConfig(log_level="DEBUG"),
     )
     engine = BacktestEngine(config=engine_config)
 
-    # Step 2: Define exchange and add it to the engine
+    # Step 4: Define exchange and add it to the engine
     EXNESS = Venue("exness")
     engine.add_venue(
         venue=EXNESS,
@@ -43,25 +68,17 @@ if __name__ == "__main__":
         default_leverage=Decimal(1),  # No leverage
     )
 
-    # Step 3: Create instrument definition and add it to the engine
+    # Step 5: Create instrument definition and add it to the engine
     EURUSD_INSTRUMENT = TestInstrumentProvider.default_fx_ccy(
         symbol="EUR/USD", venue=EXNESS
     )
     engine.add_instrument(EURUSD_INSTRUMENT)
 
-    # Step 3a: Download CSV file
-    download(
-        "https://ticks.ex2archive.com/ticks/EURUSDc/2025/09/Exness_EURUSDc_2025_09.zip"
-    )
-
-    with zipfile.ZipFile("Exness_EURUSDc_2025_09.zip", "r") as zip_ref:
-        zip_ref.extractall(Path(__file__).parent)
-
-    # Step 4a: Load bar data from CSV file -> into pandas DataFrame
-    csv_file_path = r"Exness_EURUSDc_2025_09.csv"
+    # Step 6a: Load bar data from CSV file -> into pandas DataFrame
+    csv_file_path = save_path / r"Exness_EURUSDc_2025_09.csv"
     df = pd.read_csv(csv_file_path, sep=",", decimal=".", header=0, index_col=False)
 
-    # Step 4b: Restructure DataFrame into required structure, that can be bassed `BarDataWrangler`
+    # Step 6b: Restructure DataFrame into required structure, that can be bassed `BarDataWrangler`
     #   -   5 columns: 'open', 'high', 'low', 'close', 'volume'
     #   -   'timestamp' as index
 
@@ -83,19 +100,19 @@ if __name__ == "__main__":
     # Remove volume = 0 (no data tick in that periode)
     ohlc_df_clean = ohlc_df[ohlc_df["volume"] > 0].copy()
 
-    # Step 4c: Define type of loaded bars
+    # Step 6c: Define type of loaded bars
     EURUSD_15MIN_BARTYPE = BarType.from_str(
         f"{EURUSD_INSTRUMENT.id}-15-MINUTE-MID-EXTERNAL",
     )
 
-    # Step 4d: `BarDataWrangler` converts each row object of type `Bar`
+    # Step 6d: `BarDataWrangler` converts each row object of type `Bar`
     wrangler = BarDataWrangler(EURUSD_15MIN_BARTYPE, EURUSD_INSTRUMENT)
     eurusdc_15min_bars_list: list[Bar] = wrangler.process(ohlc_df_clean)
 
-    # Step 4e: Add loaded data to the engine
+    # Step 6e: Add loaded data to the engine
     engine.add_data(eurusdc_15min_bars_list)
 
-    # Step 5: Create strategy and add it to engine
+    # Step 7: Create strategy and add it to engine
     config = EMACrossConfig(
         instrument_id=EURUSD_INSTRUMENT.id,
         bar_type=BarType.from_str(f"{EURUSD_INSTRUMENT.id}-15-MINUTE-MID-EXTERNAL"),
@@ -107,7 +124,7 @@ if __name__ == "__main__":
     strategy = EMACross(config=config)
     engine.add_strategy(strategy=strategy)
 
-    # Step 6: Run engine = Run backtest
+    # Step 8: Run engine = Run backtest
     engine.run()
 
     # Generating reports
@@ -115,5 +132,5 @@ if __name__ == "__main__":
     engine.trader.generate_order_fills_report()
     engine.trader.generate_positions_report()
 
-    # Step 7: Release system resources
+    # Step 9: Release system resources
     engine.dispose()
