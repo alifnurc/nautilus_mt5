378381e661eb48b2ab0be4269fdd2cb920dfc15c
hitagi <alifnc@proton.me>
2025-12-15 20:57:38 +0700

    chore: init pdhl strategy

---
+++ src/strategies/pdhl.py
@@ -0,0 +1,128 @@
+from decimal import Decimal
+
+import pandas as pd
+
+from nautilus_trader.config import StrategyConfig
+from nautilus_trader.model import BarType, InstrumentId
+from nautilus_trader.model.enums import OrderSide, TimeInForce
+from nautilus_trader.model.instruments import Instrument
+from nautilus_trader.model.orders import LimitOrder
+from nautilus_trader.trading.strategy import Strategy
+
+
+class PDHLConfig(StrategyConfig, frozen=True):
+    """
+    Configuration for ``PDHL`` instances.
+
+    parameters
+    ----------
+    instrument_id: InstrumentId
+        The instrument ID for the strategy.
+    bar_type: BarType
+        The bar type for the strategy.
+    trade_size: Decimal
+        The position size per trade.
+    subscribe_quote_ticks: bool, [default=False]
+        If quotes should be subscribed to.
+    request_bars: bool, [default=True]
+        If historical bars should be requested on strategy start.
+    order_time_in_force: TimeInForce, [default=TimeInForce.DAY]
+        The time in force for strategy market orders.
+    close_position_on_stop: bool, [default=False]
+        If all open positions should be closed on strategy stop.
+    """
+
+    instrument_id: InstrumentId
+    bar_type: BarType
+    trade_size: Decimal
+    subscribe_quote_ticks: bool = False
+    request_bars: bool = True
+    order_time_in_force: TimeInForce = TimeInForce.DAY
+    close_position_on_stop: bool = False
+
+
+class PDHL(Strategy):
+    """
+    An ICT strategy using previous day high and low as liquidity.
+
+    When price reach PDHL level and MSS and close inside PDHL level, then enter a position as reversal direction.
+    """
+
+    def __init__(self, config: PDHLConfig) -> None:
+        super().__init__(config)
+
+        self.instument: Instrument = None
+
+    def on_start(self) -> None:
+        """
+        Actions to be performed on strategy start.
+        """
+        self.instument = self.cache.instument(self.config.instrument_id)
+        if self.instument is None:
+            self.log.error(f"Could not find instrument for {self.config.instrument_id}")
+            self.stop()
+            return
+
+        # Get historical data
+        if self.config.request_bars:
+            self.request_bars(
+                self.config.bar_type, start=self._clock.utc_now() - pd.Timedelta(days=1)
+            )
+
+        # Subscribe to real-time data
+        self.subscribe_bars(self.config.bar_type)
+
+        if self.config.subscribe_quote_ticks:
+            self.subscribe_quote_ticks(self.config.instrument_id)
+
+    def on_stop(self) -> None:
+        """
+        Actions to be performed on strategy stop
+        """
+        if self.config.unsubscribe_data_on_stop:
+            self.unsubscribe_bars(self.config.bar_type)
+
+        if self.config.unsubscribe_data_on_stop and self.config.subscribe_quote_ticks:
+            self.unsubscribe_quote_ticks(self.config.instrument_id)
+
+    def buy(self) -> None:
+        order: LimitOrder = self.order_factory.limit(
+            instrument_id=self.config.instrument_id,
+            order_side=OrderSide.BUY,
+            quantity=0,  # Lot size calculation
+            price=0,  # Displacement price
+            time_in_force=self.config.order_time_in_force,
+            display_qty=True,
+        )
+
+        self.submit_order(order)
+
+    def sell(self) -> None:
+        order: LimitOrder = self.order_factory.limit(
+            instrument_id=self.config.instrument_id,
+            order_side=OrderSide.SELL,
+            quantity=0,  # Lot size calculation
+            price=0,  # Displacement price
+            time_in_force=self.config.order_time_in_force,
+            display_qty=True,
+        )
+
+        self.submit_order(order)
+
+    def _pdh(self) -> None:
+        pass
+
+    def _pdl(self) -> None:
+        pass
+
+    def _bsl(self) -> None:
+        pass
+
+    def _ssl(self) -> None:
+        pass
+
+    def _mss(self) -> None:
+        pass
+
+    def _fvg(self) -> None:
+        pass
