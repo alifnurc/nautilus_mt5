687f6cbcd8322e926979b709579d75877a2b49c2
hitagi <alifnc@proton.me>
2025-12-16 13:00:19 +0700

    feat: Add dotenv

---
+++ src/live.py
@@ -0,0 +1,63 @@
+import os
+from dotenv import load_dotenv
+from decimal import Decimal
+from adapters.mt5 import MT5, MT5DataClientConfig, MT5LiveDataClientFactory
+from nautilus_trader.config import (
+    LiveExecEngineConfig,
+    LoggingConfig,
+    TradingNodeConfig,
+)
+from nautilus_trader.live.node import TradingNode
+from nautilus_trader.model.data import BarType
+from nautilus_trader.model.identifiers import InstrumentId, TraderId
+
+from strategies.pdhl import PDHL, PDHLConfig
+
+load_dotenv()
+
+symbol = "EURUSD"
+
+# Configure the trading node
+config_node = TradingNodeConfig(
+    trader_id=TraderId("TESTER-001"),
+    logging=LoggingConfig(log_level="INFO", use_pyo3=True),
+    exec_engine=LiveExecEngineConfig(
+        reconciliation=False,
+    ),
+    data_clients={
+        MT5: MT5DataClientConfig(
+            account_number=os.getenv("MT5_ACCOUNT"),
+            password=os.getenv("MT5_PASSWORD"),
+            server=os.getenv("MT5_SERVER"),
+            timeout=120000,
+            rpyc_host=os.getenv("MT5_RPYC_HOST"),
+            rpyc_port=os.getenv("MT5_RPYC_PORT"),
+        )
+    },
+)
+
+# Setup and run the trading node
+node = TradingNode(config=config_node)
+
+# Configure the strategy
+config_strategy = PDHLConfig(
+    instrument_id=InstrumentId.from_str(f"{symbol}.{MT5}"),
+    bar_type=BarType.from_str(f"{symbol}.{MT5}-15-MINUTE-LAST-EXTERNAL"),
+    trade_size=Decimal(0.01),
+)
+strategy = PDHL(config=config_strategy)
+
+# Add the strategy to the node
+node.trader.add_strategy(strategy=strategy)
+
+# Register the data client factory
+node.add_data_client_factory(MT5, MT5LiveDataClientFactory)
+node.build()
+
+# Run the node
+try:
+    node.run()
+except KeyboardInterrupt:
+    node.stop()
+finally:
+    node.dispose()
--- tests/test_pymt5linux.py
+++ tests/test_pymt5linux.py
@@ -1,9 +1,34 @@
+import os
 from pymt5linux import MetaTrader5
+from dotenv import load_dotenv
 
-print("Establishing connection...")
-mt5 = MetaTrader5(host="localhost", port=18847)
+load_dotenv()
 
-mt5.initialize()
 
-print("Closing connection...")
-mt5.shutdown()
+def test_rpyc_connection():
+    mt5_login = int(os.getenv("MT5_ACCOUNT"))
+    mt5_password = os.getenv("MT5_PASSWORD")
+    mt5_server = os.getenv("MT5_SERVER")
+    mt5_host = os.getenv("MT5_RPYC_HOST")
+    mt5_port = int(os.getenv("MT5_RPYC_PORT"))
+
+    print("Establishing connection...")
+    mt5 = MetaTrader5(host=mt5_host, port=mt5_port)
+
+    # Debug
+    print(mt5_login)
+
+    print("Initialize MetaTrader5 terminal...")
+    if not mt5.initialize(login=mt5_login, password=mt5_password, server=mt5_server):
+        print(f"Initialize() failed, error code = {mt5.last_error()}")
+        quit()
+
+    print(f"Terminal info: {mt5.terminal_info()}\n")
+    print(f"Account info: {mt5.account_info()}\n")
+
+    print("Closing connection...")
+    mt5.shutdown()
+
+
+if __name__ == "__main__":
+    test_rpyc_connection()
