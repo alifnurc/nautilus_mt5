8e7a427cb2c8dcf911f705e9ce44a98232aad66c
hitagi <alifnc@proton.me>
2026-01-01 19:11:14 +0700

    feat: Update factories

--- src/adapters/mt5/factories.py
+++ src/adapters/mt5/factories.py
@@ -1,13 +1,14 @@
 import asyncio
 from functools import lru_cache
 
+from adapters.mt5.client import AsyncMT5RPyCClient
 from adapters.mt5.config import MT5ClientConfig
 from adapters.mt5.data import MT5DataClient
+from adapters.mt5.execution import MT5ExecutionClient
 from adapters.mt5.providers import MT5InstrumentProvider
 from nautilus_trader.common.component import LiveClock, MessageBus
 from nautilus_trader.cache.cache import Cache
-from nautilus_trader.config import InstrumentProviderConfig, LiveExecClientConfig
-from nautilus_trader.core import nautilus_pyo3
+from nautilus_trader.config import InstrumentProviderConfig
 from nautilus_trader.live.factories import (
     LiveDataClientFactory,
     LiveExecClientFactory,
@@ -15,8 +16,15 @@ from nautilus_trader.live.factories import (
 
 
 @lru_cache(maxsize=1)
-def get_mt5_instrument_provider() -> MT5InstrumentProvider:
-    return MT5InstrumentProvider(client=client, config=config)
+def get_mt5_rpyc_client(config: MT5ClientConfig) -> AsyncMT5RPyCClient:
+    return AsyncMT5RPyCClient(config=config)
+
+
+@lru_cache(maxsize=1)
+def get_mt5_instrument_provider(
+    client: AsyncMT5RPyCClient, active_only: bool, config: InstrumentProviderConfig
+) -> MT5InstrumentProvider:
+    return MT5InstrumentProvider(client=client, active_only=active_only, config=config)
 
 
 class MT5LiveDataClientFactory(LiveDataClientFactory):
@@ -24,12 +32,60 @@ class MT5LiveDataClientFactory(LiveDataClientFactory):
     Provides a MT5 live data client factory.
     """
 
-    pass
+    @staticmethod
+    def create(
+        loop: asyncio.AbstractEventLoop,
+        name: str,
+        config: MT5ClientConfig,
+        msgbus: MessageBus,
+        cache: Cache,
+        clock: LiveClock,
+    ) -> MT5DataClient:
+        client = get_mt5_rpyc_client(MT5ClientConfig)
+
+        provider = get_mt5_instrument_provider(
+            client=client, active_only=True, config=config.instrument_provider
+        )
+
+        return MT5DataClient(
+            loop=loop,
+            client=client,
+            msgbus=msgbus,
+            cache=cache,
+            clock=clock,
+            instrument_provider=provider,
+            config=config,
+            name=name,
+        )
 
 
-class MT5LiveExecClientFactory(LiveExecClientConfig):
+class MT5LiveExecClientFactory(LiveExecClientFactory):
     """
     Provides a MT5 live execution client factory.
     """
 
-    pass
+    @staticmethod
+    def create(
+        loop: asyncio.AbstractEventLoop,
+        name: str,
+        config: MT5ClientConfig,
+        msgbus: MessageBus,
+        cache: Cache,
+        clock: LiveClock,
+    ) -> MT5ExecutionClient:
+        client = get_mt5_rpyc_client(MT5ClientConfig)
+
+        provider = get_mt5_instrument_provider(
+            client=client, active_only=True, config=config.instrument_provider
+        )
+
+        return MT5ExecutionClient(
+            loop=loop,
+            client=client,
+            msgbus=msgbus,
+            cache=cache,
+            clock=clock,
+            instrument_provider=provider,
+            config=config,
+            name=name,
+        )
